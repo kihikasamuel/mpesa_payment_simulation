51c9bf08d0c641368395c19a2d823a50dae56476
Samirose <summitros@gmail.com>
2018-12-21 00:39:47 +0300

    5th commit

--- payment.class.php
+++ payment.class.php
@@ -9,7 +9,7 @@ class Payment
 	
 	function __construct()
 	{
-		$url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';.
+		$url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
 	}
 
 	function getAuthentication()
@@ -20,7 +20,7 @@ class Payment
 		curl_setopt($curl, CURLOPT_URL, $url);
 		$credentials = base64_encode('cNJHuybnZTKGQFv34yl7Ywa9yWFtmXoX:tOMWnnvIU8FFoi29');
 		curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Basic '.$credentials));
-		curl_setopt($curl, CURLOPT_HEADER, true);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
 		$curl_response = curl_exec($curl);
