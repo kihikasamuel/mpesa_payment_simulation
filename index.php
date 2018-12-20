bc0cc3305a561401cf3b359b0708b5ad0e913bbb
Samirose <summitros@gmail.com>
2018-12-21 00:16:52 +0300

    class included

--- index.php
+++ index.php
@@ -1,33 +1,7 @@
 <?php
+	require_once('payment.class.php');
 
-/**
- * @author root
- *on 12.20.2018
- */
-class Payment
-{
-	
-	// function __construct()
-	// {
-	// 	# code...
-	// }
+	$pay = Payment();
 
-	function getAuthentication()
-	{
-		$url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
-
-		$curl = curl_init();
-		curl_setopt($curl, CURLOPT_URL, $url);
-		$credentials = base64_encode('cNJHuybnZTKGQFv34yl7Ywa9yWFtmXoX:tOMWnnvIU8FFoi29');
-		curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Basic '.$credentials));
-		curl_setopt($curl, CURLOPT_HEADER, true);
-		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-
-		$curl_response = curl_exec($curl);
-
-		echo json_decode($curl_response);
-
-	}
-}
-
+	echo getAuthentication();
 ?>
